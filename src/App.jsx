01eb0a559aca544d6efc7972c15aa04b64d68294
Viktotovich <148013724+Viktotovich@users.noreply.github.com>
2025-01-19 17:22:41 +0400

    Completed CH1

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,7 @@
 // Don't use anchor tags, instead use Link
-import { Link } from "react-router-dom";
 
 function App() {
-  return (
-    <>
-      <nav>
-        <h1>Test Route</h1>
-        <hr />
-        <ul>
-          <li>
-            <Link to="test">Take me to Test!</Link>
-          </li>
-        </ul>
-      </nav>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
